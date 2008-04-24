978831cecb66b1c5e7184773e875f36da798da84
tstoenner <tstoenner@f3dbc544-a14b-0410-9061-999cd7a3f3ce>
2008-04-24 17:00:46 +0000

    First Creation of Pavel2 Project.

---
+++ trunk/trunk/Pavel2/Framework/CSVParser.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.IO;
+
+namespace Pavel2.Framework {
+    public class CSVParser : IParser {
+
+        StreamReader stream;
+        Char delimiter = ',';
+        Column[] columns;
+
+        public Column[] Columns {
+            get { return columns; }
+            set { columns = value; }
+        }
+        List<DiscretePoint>[] tmp;
+
+        public Char Delimiter {
+            get { return delimiter; }
+            set { delimiter = value; }
+        }
+
+        public StreamReader File {
+            get {
+                return stream;
+            }
+            set {
+                stream = value;
+            }
+        }
+
+        public void Parse() {
+            int columnNumber = NumerOfColumns();
+            columns = new Column[columnNumber];
+            tmp = new List<DiscretePoint>[columnNumber];
+            for (int i = 0; i < columnNumber; i++) {
+                tmp[i] = new List<DiscretePoint>();
+                columns[i] = new Column();
+            }
+            String line;
+            String[] lineSplit;
+            while ((line = stream.ReadLine()) != null) {
+                lineSplit = line.Split(delimiter);
+                for (int i = 0; i < columnNumber; i++) {
+                    tmp[i].Add(new DiscretePoint(lineSplit[i]));
+                }
+            }
+            for (int i = 0; i < tmp.Length; i++) {
+                columns[i].Points = tmp[i].ToArray(); ;
+            }
+            MainData.AddColumns(columns);
+        }
+
+        //TODO: Erste Zeile damit schon weg!!!
+        private int NumerOfColumns() {
+            return stream.ReadLine().Split(delimiter).Length;
+        }
+
+    }
+}
---
+++ trunk/trunk/Pavel2/GUI/MainWindow.xaml.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.IO;
+using Pavel2.Framework;
+
+namespace Pavel2.GUI
+{
+    /// <summary>
+    /// Interaktionslogik für MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window {
+        public MainWindow() {
+            InitializeComponent();
+            InitDirectoryTree();
+        }
+
+        private void InitDirectoryTree() {
+            foreach (DriveInfo drive in DriveInfo.GetDrives()) {
+                TreeViewItem item = new TreeViewItem();
+                item.Tag = drive;
+                item.Header = drive.ToString();
+
+                item.Items.Add("*");
+                directoryTree.Items.Add(item);
+            }
+        }
+
+        private void directoryTree_Expanded(object sender, RoutedEventArgs e) {
+            TreeViewItem item = (TreeViewItem)e.OriginalSource;
+            item.Items.Clear();
+            DirectoryInfo dir;
+            if (item.Tag is DriveInfo) {
+                DriveInfo drive = (DriveInfo)item.Tag;
+                dir = drive.RootDirectory;
+            } else {
+                dir = (DirectoryInfo)item.Tag;
+            }
+            try {
+                foreach (DirectoryInfo subDir in dir.GetDirectories()) {
+                    TreeViewItem newItem = new TreeViewItem();
+                    newItem.Tag = subDir;
+                    newItem.Header = subDir.ToString();
+                    newItem.Items.Add("*");
+                    item.Items.Add(newItem);
+                }
+            } catch {
+                // An exception could be thrown in this code if you don't
+                // have sufficient security permissions for a file or directory.
+                // You can catch and then ignore this exception.
+            }
+        }
+
+        private void directoryTree_SelectedItemChanged(object sender, RoutedPropertyChangedEventArgs<object> e) {
+            TreeViewItem item = (TreeViewItem)directoryTree.SelectedItem;
+            if (item.Tag is DirectoryInfo) {
+                fileList.ItemsSource = ((DirectoryInfo)item.Tag).GetFiles();
+            } else if (item.Tag is DriveInfo) {
+                fileList.ItemsSource = ((DriveInfo)item.Tag).RootDirectory.GetFiles();
+            }
+        }
+
+        private void button5_Click(object sender, RoutedEventArgs e) {
+            FileInfo file = (FileInfo)fileList.SelectedItem;
+            CSVParser csv = new CSVParser();
+            csv.File = file.OpenText();
+            csv.Delimiter = ';';
+            csv.Parse();
+            DataGrid dataGrid = new DataGrid(csv.Columns, file.Name);
+            projectTree.Items.Add(dataGrid);
+        }
+
+        private void projectTree_SelectedItemChanged(object sender, RoutedPropertyChangedEventArgs<object> e) {
+            DataGrid dataGrid = (DataGrid)projectTree.SelectedItem;
+            listViewTable.ItemsSource = dataGrid.Columns[0].Points;
+            listViewTable.DisplayMemberPath = "Data";
+        }
+    }
+}
