bf90dcebb41f1eee61973245836e494ae9ee983e
tstoenner <tstoenner@f3dbc544-a14b-0410-9061-999cd7a3f3ce>
2008-04-24 21:49:51 +0000

    First implementation of Parser Structure.

---
+++ trunk/Pavel2/Framework/ParserManagement.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.IO;
+
+namespace Pavel2.Framework {
+    public static class ParserManagement {
+
+        private static List<IParser> parserList = new List<IParser>();
+
+        public static DataGrid GetDataGrid(StreamReader stream) {
+            UpdateParserList();
+            Column[] columns;
+            foreach (IParser parser in parserList) {
+                columns = parser.Parse(stream);
+                if (null != columns) {
+                    return MainData.AddColumns(columns);
+                }
+            }
+            return null;
+        }
+
+        //TODO: Automatisch alle die IParser implementieren holen
+        private static void UpdateParserList() { 
+            parserList.Clear();
+            parserList.Add(new CSVParser());
+        }
+
+    }
+}
