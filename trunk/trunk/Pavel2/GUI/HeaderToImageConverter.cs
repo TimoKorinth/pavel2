2ab91d00f89585a1a367d603986df9962c169515
tstoenner <tstoenner@f3dbc544-a14b-0410-9061-999cd7a3f3ce>
2008-05-10 10:47:54 +0000

    Showing icons for DirectoryTreeView

---
+++ trunk/trunk/Pavel2/GUI/HeaderToImageConverter.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows.Data;
+using System.Windows.Media.Imaging;
+
+namespace Pavel2.GUI {
+    [ValueConversion(typeof(string), typeof(bool))]
+    public class HeaderToImageConverter : IValueConverter {
+
+        public object Convert(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture) {
+            if ((value as string).Contains(@"\")) {
+                Uri uri = new Uri("pack://application:,,,/GUI/Icons/HDD.png");
+                BitmapImage source = new BitmapImage(uri);
+                return source;
+            } else {
+                Uri uri = new Uri("pack://application:,,,/GUI/Icons/Folder.png");
+                BitmapImage source = new BitmapImage(uri);
+                return source;
+            }
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture) {
+            throw new NotImplementedException();
+        }
+    }
+}
