c362e4ba99766deab96ed407263de6d8bd9eb7de
tstoenner <tstoenner@f3dbc544-a14b-0410-9061-999cd7a3f3ce>
2008-05-19 22:21:33 +0000

    Implement correct view of director Tree View with nice icons.

---
+++ trunk/Pavel2/GUI/HeaderToHeaderConverter.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows.Data;
+using System.Windows.Media.Imaging;
+
+namespace Pavel2.GUI {
+    public class HeaderToHeaderConverter : IValueConverter {
+
+        public object Convert(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture) {
+            if (value is String) {
+                String header = (String)value;
+                header = header.Remove(0, 2);
+                return header;
+            }
+            return value;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture) {
+            throw new NotImplementedException();
+        }
+    }
+}
--- trunk/Pavel2/GUI/HeaderToImageConverter.cs
+++ trunk/Pavel2/GUI/HeaderToImageConverter.cs
@@ -9,15 +9,23 @@ namespace Pavel2.GUI {
     public class HeaderToImageConverter : IValueConverter {
 
         public object Convert(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture) {
-            if ((value as string).Contains(@"\")) {
-                Uri uri = new Uri("pack://application:,,,/GUI/Icons/HDD.png");
-                BitmapImage source = new BitmapImage(uri);
-                return source;
-            } else {
-                Uri uri = new Uri("pack://application:,,,/GUI/Icons/Folder.png");
-                BitmapImage source = new BitmapImage(uri);
-                return source;
+            if (value is String) {
+                String header = (String)value;
+                if (header.StartsWith("H#")) {
+                    Uri uri = new Uri("pack://application:,,,/GUI/Icons/HDD.png");
+                    BitmapImage source = new BitmapImage(uri);
+                    return source;
+                } else if (header.StartsWith("D#")) {
+                    Uri uri = new Uri("pack://application:,,,/GUI/Icons/Folder.png");
+                    BitmapImage source = new BitmapImage(uri);
+                    return source;
+                } else if (header.StartsWith("F#")) {
+                    Uri uri = new Uri("pack://application:,,,/GUI/Icons/Document.png");
+                    BitmapImage source = new BitmapImage(uri);
+                    return source;
+                }
             }
+            return value;
         }
 
         public object ConvertBack(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture) {
