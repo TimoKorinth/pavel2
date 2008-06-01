00d81549f1441a2d04b51b2edd0d781fd772e4f0
tstoenner <tstoenner@f3dbc544-a14b-0410-9061-999cd7a3f3ce>
2008-06-01 23:35:58 +0000

    Added two additional DataTemplates for the property Grid

---
+++ trunk/Pavel2 GUI Test/Pavel2/GUI/EnumTypeConverter.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows.Data;
+
+namespace Pavel2.GUI {
+    public class EnumTypeConverter : IValueConverter {
+
+        public object Convert(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture) {
+            return Enum.GetValues(value.GetType());
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture) {
+            throw new NotSupportedException();
+        }
+    }
+}
