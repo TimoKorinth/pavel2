28236c66b671f54e64619155abd055433b972d37
tstoenner <tstoenner@f3dbc544-a14b-0410-9061-999cd7a3f3ce>
2008-06-02 15:08:29 +0000

    First implementation of Link List

---
+++ trunk/Pavel2 GUI Test/Pavel2/GUI/LinkList.xaml.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using Pavel2.Framework;
+
+namespace Pavel2.GUI {
+    /// <summary>
+    /// Interaktionslogik für LinkList.xaml
+    /// </summary>
+    public partial class LinkList : UserControl {
+        
+        public LinkList() {
+            InitializeComponent();
+        }
+
+        private void linkTreeView_Drop(object sender, DragEventArgs e) {
+        }
+
+        private void linkTreeView_DragEnter(object sender, DragEventArgs e) {
+        }
+
+        private void linkTreeView_DragLeave(object sender, DragEventArgs e) {
+        }
+
+        private void linkTreeView_DragOver(object sender, DragEventArgs e) {
+
+        }
+
+        private void newItemLabel_Drop(object sender, DragEventArgs e) {
+            TreeViewItem tvItem = e.Data.GetData(typeof(TreeViewItem)) as TreeViewItem;
+            if (tvItem == null) return;
+            if (tvItem.Tag is DataProjectTreeItem) {
+                DataProjectTreeItem dPTI = tvItem.Tag as DataProjectTreeItem;
+                TreeViewItem newItem = new TreeViewItem();
+                newItem.Header = tvItem.Header;
+                linkTreeView.Items.Add(newItem);
+            }
+        }
+    }
+}
