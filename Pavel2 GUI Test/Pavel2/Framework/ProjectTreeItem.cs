c710498027d8c086907af3d5c23ec873edc37e80
tstoenner <tstoenner@f3dbc544-a14b-0410-9061-999cd7a3f3ce>
2008-06-02 22:41:20 +0000

    New implementation of preview with the RenderTargetBitmap class!!!!! Now fully functional!!!!!

--- Pavel2 GUI Test/Pavel2/Framework/ProjectTreeItem.cs
+++ Pavel2 GUI Test/Pavel2/Framework/ProjectTreeItem.cs
@@ -3,12 +3,28 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using Pavel2.GUI;
+using System.Windows.Media.Imaging;
+using System.Windows.Media;
+using System.Windows;
 
 namespace Pavel2.Framework {
     public abstract class ProjectTreeItem {
 
         private String header;
         private Visualization lastVisualization;
+        RenderTargetBitmap screenshot;
+
+        public RenderTargetBitmap Screenshot {
+            get { return screenshot; }
+            set { screenshot = value; }
+        }
+
+        public void TakeScreenShot() {
+            if (lastVisualization == null) return;
+            FrameworkElement vis = (FrameworkElement)lastVisualization;
+            screenshot = new RenderTargetBitmap((int)vis.ActualWidth, (int)vis.ActualHeight, 96, 96, PixelFormats.Pbgra32);
+            screenshot.Render(vis);
+        }
 
         public Visualization LastVisualization {
             get { return lastVisualization; }
