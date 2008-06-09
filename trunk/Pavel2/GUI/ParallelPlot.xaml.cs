6e7ce51b1c4af18f9a5186a7000b37935c8fd60c
tstoenner <tstoenner@f3dbc544-a14b-0410-9061-999cd7a3f3ce>
2008-06-09 18:20:45 +0000

    First implementation of parallelPlot with openGl

--- trunk/Pavel2/GUI/ParallelPlot.xaml.cs
+++ trunk/Pavel2/GUI/ParallelPlot.xaml.cs
@@ -4,6 +4,8 @@ using System.Windows.Controls;
 using Tao.OpenGl;
 using Tao.Platform.Windows;
 using System.Windows.Forms.Integration;
+using Pavel2.Framework;
+using System.Windows.Media;
 
 namespace Pavel2.GUI {
     /// <summary>
@@ -14,13 +16,22 @@ namespace Pavel2.GUI {
         internal class OpenGLRenderWind : SimpleOpenGlControl {
             public OpenGLRenderWind() {
                 this.InitializeContexts();
-                // Should I explicitly set this.Size here?
+                Gl.glClearColor(1.0f, 1.0f, 1.0f, 0.0f);
+                Gl.glShadeModel(Gl.GL_FLAT);
+                Gl.glEnable(Gl.GL_LINE_SMOOTH);
+                Gl.glEnable(Gl.GL_BLEND);
+                Gl.glBlendFunc(Gl.GL_SRC_ALPHA, Gl.GL_ONE_MINUS_SRC_ALPHA);
+                Gl.glDisable(Gl.GL_CULL_FACE);
+                Gl.glMatrixMode(Gl.GL_PROJECTION);
+                Gl.glLoadIdentity();
+                Gl.glOrtho(0.0, 1.0, 0.0, 1.0, -1.0, 1.0);
             }
         }
 
         WindowsFormsHost host;
         System.Windows.Forms.Control wfPA;
-        static double A, B, C, D;
+        DataGrid dataGrid;
+        double step;
 
         public ParallelPlot() {
             InitializeComponent();
@@ -28,35 +39,66 @@ namespace Pavel2.GUI {
             wfPA = new OpenGLRenderWind();
             wfPA.Paint += DrawData;
             host.Child = wfPA;
-            this.openGlCanvas.Children.Add(host);
-            Gl.glClearColor(1.0f, 0.0f, 0.0f, 0.0f);
-            Gl.glColor3f(1.0f, 1.0f, 1.0f);
-            Gl.glPointSize(2.0f);
-            Gl.glMatrixMode(Gl.GL_PROJECTION);
-            Gl.glLoadIdentity();
-            Gl.glOrtho(0.0, 1.0, 0.0, 1.0, -1.0, 1.0);
+            openGlCanvas.Children.Add(host);
+            SetViewPort();
+        }
+
+        private void DrawLines() {
+            Gl.glColor3f(0.043f, 0.729f, 0.878f);
+            Gl.glLineWidth(1f);
+            if (dataGrid == null) return;
+            for (int row = 0; row < dataGrid.Columns[dataGrid.MaxColumn].Points.Length; row++) {
+                Gl.glBegin(Gl.GL_LINE_STRIP);
+                for (int col = 0; col < dataGrid.Columns.Length; col++) {
+                    double nValue = Normalize(dataGrid.Columns[col].Points[row].DoubleData, dataGrid.Columns[col]);
+                    Gl.glVertex2d(step*col, nValue);
+                }
+                Gl.glEnd();
+            }
+        }
+
+        private double Normalize(double value, Column col) {
+            if ((col.Max - col.Min) == 0) {
+                return 0.5;
+            }
+            return ((value - col.Min) / (col.Max - col.Min));
+        }
+
+        private void DrawAxes() {
+            
         }
 
         private void DrawData(object sender, System.Windows.Forms.PaintEventArgs e) {
+            RenderScene();
+        }
+
+        private void RenderScene() {
             Gl.glClear(Gl.GL_COLOR_BUFFER_BIT);
-            Gl.glBegin(Gl.GL_LINE_STRIP);
-            double x, y;
-            Random rand = new Random();
-            for (double i = 0; i < 100; i++) {
-                y = (float)(rand.NextDouble());
-                x = (float)i / (100 / 10);
-                Gl.glVertex2d(x, y);
-            }
-            Gl.glEnd();
+
+            //DrawAxes();
+            DrawLines();
+
             Gl.glFlush();
+        }
+
+
+        private void SetViewPort() {
+            Gl.glViewport(0, 0, (int)openGlCanvas.ActualWidth, (int)openGlCanvas.ActualHeight);
         }
 
         #region Visualization Member
 
         public void Render() {
+            SetViewPort();
+            dataGrid = MainData.CurrentDataGrid;
+            if (dataGrid == null) return;
+            step = (double)1 / (dataGrid.Columns.Length - 1);
+            RenderScene();
+            SetViewPort();
         }
 
         public void RenderAfterResize() {
+            SetViewPort();
         }
 
         #endregion
