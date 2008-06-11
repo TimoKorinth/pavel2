c1bacb07face54ae3e77ce1a4b618c15acb6a40c
tstoenner <tstoenner@f3dbc544-a14b-0410-9061-999cd7a3f3ce>
2008-06-11 22:01:45 +0000

    Implement MultiTreeView

--- trunk/Pavel2/GUI/MultiTreeView.cs
+++ trunk/Pavel2/GUI/MultiTreeView.cs
@@ -3,8 +3,62 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Windows.Controls;
+using System.Windows.Input;
+using System.Windows;
+using System.Windows.Media;
 
 namespace Pavel2.GUI {
     public class MultiTreeView : TreeView {
+
+        private List<TreeViewItem> selItems = new List<TreeViewItem>();
+
+        private bool CtrlPressed {
+            get { return System.Windows.Input.Keyboard.IsKeyDown(Key.LeftCtrl); }
+        }
+
+        public TreeViewItem SelectedTreeViewItem {
+            get {
+                if (selItems.Count > 0) return selItems[0];
+                return null; 
+            }
+        }
+
+        public MultiTreeView() : base() {
+            this.SelectedItemChanged += TreeView_SelectedItemChanged;
+        }
+
+        private void TreeView_SelectedItemChanged(object sender, RoutedPropertyChangedEventArgs<object> e) {
+            TreeViewItem item = this.SelectedItem as TreeViewItem;
+            if (item == null) return;
+            item.IsSelected = false;
+            if (!CtrlPressed) {
+                List<TreeViewItem> selectedTreeViewItemList = new List<TreeViewItem>();
+                foreach (TreeViewItem treeViewItem1 in selItems) {
+                    selectedTreeViewItemList.Add(treeViewItem1);
+                }
+
+                foreach (TreeViewItem treeViewItem1 in selectedTreeViewItemList) {
+                    Deselect(treeViewItem1);
+                }
+            }
+            ChangeSelectedState(item);
+        }
+
+        void Deselect(TreeViewItem treeViewItem) {
+            treeViewItem.Background = Brushes.White;
+            treeViewItem.Foreground = Brushes.Black;
+            selItems.Remove(treeViewItem);
+        }
+
+        void ChangeSelectedState(TreeViewItem treeViewItem) {
+            if (!selItems.Contains(treeViewItem)) {
+                treeViewItem.Background = Brushes.Silver;
+                treeViewItem.Foreground = Brushes.Black;
+                selItems.Add(treeViewItem);
+            } else {
+                Deselect(treeViewItem);
+            }
+        }
+
     }
 }
