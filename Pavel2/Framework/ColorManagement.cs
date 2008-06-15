25382729bd106ed694aaccc7e54639b1dcb33f59
tstoenner <tstoenner@f3dbc544-a14b-0410-9061-999cd7a3f3ce>
2008-06-15 18:21:40 +0000

    Add ColorManagement

---
+++ Pavel2/Framework/ColorManagement.cs
@@ -0,0 +1,159 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Drawing;
+
+namespace Pavel2.Framework {
+    public static class ColorManagement {
+
+        #region Fields
+
+        private static List<ColorOGL> selectionColors;
+        private static ColorOGL backgroundColor = new ColorOGL(Color.Black);
+        private static ColorOGL axesColor = new ColorOGL(Color.LightGray);
+        private static ColorOGL descriptionColor = new ColorOGL(Color.LightGray);
+
+        #endregion
+
+        #region Properties
+
+        /// <value>Gets the color for descriptions</value>
+        public static ColorOGL DescriptionColor { get { return ColorManagement.descriptionColor; } }
+
+        /// <value>Gets the color for axes</value>
+        public static ColorOGL AxesColor { get { return ColorManagement.axesColor; } }
+
+        /// <value>Gets the color for the background</value>
+        public static ColorOGL BackgroundColor { get { return backgroundColor; } }
+
+        /// <value>Gets the color for unselected points</value>
+        public static ColorOGL UnselectedColor {
+            get {
+                if (null == selectionColors) { InitializeSelectionColors(2); }
+                return GetColor(0);
+            }
+        }
+
+        /// <value>Gets the color for currently selected points</value>
+        public static ColorOGL CurrentSelectionColor {
+            get {
+                if (null == selectionColors) { InitializeSelectionColors(2); }
+                return GetColor(1);
+            }
+        }
+
+        #endregion
+
+        #region Methods
+
+        /// <summary>
+        /// Initializes the selectionColors.
+        /// </summary>
+        /// <param name="count">Total number of colors</param>
+        private static void InitializeSelectionColors(int count) {
+            selectionColors = new List<ColorOGL>();
+            FillSelectionColors(count, true);
+        }
+
+        /// <summary>
+        /// Returns a color at a position. If no color exists at this position
+        /// the selectionColors list is expanded.
+        /// </summary>
+        /// <param name="position">Position of the Color.</param>
+        /// <returns>ColorOGL</returns>
+        public static ColorOGL GetColor(int position) {
+            if (selectionColors == null) {
+                InitializeSelectionColors(position + 1);
+            }
+            if (position >= selectionColors.Count) {
+                FillSelectionColors(position + 1, false);
+            }
+            return selectionColors[position];
+        }
+
+        /// TODO: Change from precomputed array to inline computation???
+        /// <summary>
+        /// Fills the selectionColors list with colors, which are optimized
+        /// for good readability. selectionColors will be filled up to the
+        /// next number that isgreater or eaqual to count and a power of two
+        /// </summary>
+        /// <param name="count">Total number of colors</param>
+        /// <param name="clearList">Recreate list by clearing it before refill</param>
+        private static void FillSelectionColors(int count, bool clearList) {
+            if (clearList || selectionColors.Count <= 2) {
+                selectionColors.Clear();
+                float unselected = 180f;    //blue-green
+                float selected = 0f;      //red
+                selectionColors.Add(HSVToRGB(unselected, 1, 0.78f));
+                selectionColors.Add(HSVToRGB(selected, 1, 0.78f));
+            }
+            // The colorvalues are taken from HSV-Colorspace, starting with
+            // angels 0° and 180° and iterating by bisecting each partition:
+            // basis: 2, 3, 4, 5, ...
+            float basis = (float)(Math.Ceiling(Math.Log((double)selectionColors.Count + 1.0, 2.0)));
+            // 4, 8, 16, 32, ...
+            int index = (int)Math.Pow(2.0, basis);
+            // (1, 3), (1, 3, 5, 7), (1, 3, 5, 7, 9, 11, 13, 15), ...
+            int i = (int)Math.Pow(2.0, basis - 1) - selectionColors.Count + 1;
+            while (selectionColors.Count < count) {
+                for (; i < index /*&& selectionColors.Count < count*/; i += 2) {
+                    float h = (360.0f / index) * i;       //Values: 0, 180, 90, 270, 45, 135
+                    float s = 0.5f + (((basis - 2f) * 0.25f) % 0.5f); // Values: 0.5, 0.75, 1.0
+                    float v = 0.78f + (((basis - 2f) * 0.2f) % 0.2f);  // Values: 0.78, 0.98
+                    selectionColors.Add(HSVToRGB(h, s, v));
+                }
+                index *= 2;     // Next Index
+                basis++;        // Correxponding Base
+                i = 1;          // Restart local position in this index
+            }
+        }
+
+        /// <summary>
+        /// Converts HSV Colors to RGB Colors.
+        /// </summary>
+        /// <param name="h">Hue (0 to 360)</param>
+        /// <param name="s">Saturation (0 to 1)</param>
+        /// <param name="v">Value (0 to 1)</param>
+        /// <returns>OpenGL Color</returns>
+        public static ColorOGL HSVToRGB(float h, float s, float v) {
+            ColorOGL color = new ColorOGL();
+            int i;
+            float f, p, q, t;
+            if (s == 0) {
+                color.Color = Color.FromArgb(0, (int)(v * 255), (int)(v * 255), (int)(v * 255));
+                return color;
+            }
+            h /= 60;
+            i = (int)Math.Floor(h);
+            f = h - i;
+            p = v * (1 - s);
+            q = v * (1 - s * f);
+            t = v * (1 - s * (1 - f));
+            switch (i) {
+                case 0:
+                    color.Color = Color.FromArgb(0, (int)(v * 255), (int)(t * 255), (int)(p * 255));
+                    break;
+                case 1:
+                    color.Color = Color.FromArgb(0, (int)(q * 255), (int)(v * 255), (int)(p * 255));
+                    break;
+                case 2:
+                    color.Color = Color.FromArgb(0, (int)(p * 255), (int)(v * 255), (int)(t * 255));
+                    break;
+                case 3:
+                    color.Color = Color.FromArgb(0, (int)(p * 255), (int)(q * 255), (int)(v * 255));
+                    break;
+                case 4:
+                    color.Color = Color.FromArgb(0, (int)(t * 255), (int)(p * 255), (int)(v * 255));
+                    break;
+                default:
+                    color.Color = Color.FromArgb(0, (int)(v * 255), (int)(p * 255), (int)(q * 255));
+                    break;
+            }
+            return color;
+        }
+
+        #endregion
+
+    }
+}
---
+++ Pavel2/Framework/ColorOGL.cs
@@ -0,0 +1,237 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Drawing;
+using System.ComponentModel;
+
+namespace Pavel2.Framework {
+    public class ColorOGL {
+
+        #region Fields
+
+        private Color color;
+        private float[] rgbaFloat;
+        private String description;
+
+        #endregion
+
+        #region Properties
+
+        /// <value>Gets the alpha value of the ColorOGL as a float or sets it</value>
+        [Browsable(false)]
+        public float A {
+            get { return rgbaFloat[3]; }
+            private set { rgbaFloat[3] = value; color = colorFromFloat(rgbaFloat); }
+        }
+
+        /// <value>Gets the red value of the ColorOGL as a float or sets it</value>
+        [Browsable(false)]
+        public float R {
+            get { return rgbaFloat[0]; }
+            private set { rgbaFloat[0] = value; color = colorFromFloat(rgbaFloat); }
+        }
+
+        /// <value>Gets the green value of the ColorOGL as a float or sets it</value>
+        [Browsable(false)]
+        public float G {
+            get { return rgbaFloat[1]; }
+            private set { rgbaFloat[1] = value; color = colorFromFloat(rgbaFloat); }
+        }
+
+        /// <value>Gets the blue value of the ColorOGL as a float or sets it</value>
+        [Browsable(false)]
+        public float B {
+            get { return rgbaFloat[2]; }
+            private set { rgbaFloat[2] = value; color = colorFromFloat(rgbaFloat); }
+        }
+
+        /// <value>Gets a float-array of the RGB values of the ColorOGL</value>
+        [Browsable(false)]
+        public float[] RGB {
+            get { return new float[] { rgbaFloat[0], rgbaFloat[1], rgbaFloat[2] }; }
+        }
+
+        /// <value>Gets a float-array of the RGBA values of the ColorOGL</value>
+        [Browsable(false)]
+        public float[] RGBA { get { return rgbaFloat; } }
+
+        /// <summary>
+        /// Gets a float-array of the RGB values of the ColorOGL, with an alpha value mixed in on-the-fly
+        /// </summary>
+        /// <param name="alpha">The desired alpha-value</param>
+        /// <returns>A float-array of the RGB values of the ColorOGL, with an alpha value mixed in on-the-fly</returns>
+        public float[] RGBwithA(float alpha) {
+            float[] retval = new float[4];
+            rgbaFloat.CopyTo(retval,0);
+            retval[3] = alpha;
+            return retval;
+        }
+
+        /// <value>Gets the ColorOGL as Color or sets it</value>
+        [RefreshProperties(RefreshProperties.Repaint)]
+        [NotifyParentProperty(true)]
+        public Color Color {
+            get { return color; }
+            set {
+                color = value;
+                rgbaFloat = floatFromColor(color);
+            }
+        }
+
+        /// <value>Gets a description of the ColorOGL or sets it</value>
+        public String Description {
+            get { return description; }
+            set { description = value; }
+        }
+
+        #endregion
+
+        #region Constructors
+
+        /// <summary>
+        /// Sets the color to white and the description to "(none)".
+        /// </summary>
+        public ColorOGL() {
+            Color = Color.White;
+            Description = "(none)";
+        }
+
+        /// <summary>
+        /// Sets the color to <paramref name="color"/> and the description to the Name of <paramref name="color"/>.
+        /// </summary>
+        /// <param name="color">Color to be set</param>
+        public ColorOGL(Color color) {
+            Color = color;
+            Description = color.Name;
+        }
+
+        /// <summary>
+        /// Sets the color to <paramref name="color"/> and the description to <paramref name="desc"/>.
+        /// </summary>
+        /// <param name="color">Color to be set</param>
+        /// <param name="desc">Description for the color</param>
+        public ColorOGL(Color color, String desc) {
+            Color = color;
+            Description = desc;
+        }
+
+        #endregion
+
+        #region Methods
+
+        /// <summary>
+        /// Calculates the float-values of the given Color.
+        /// </summary>
+        /// <param name="color">Given Color</param>
+        /// <returns>Float-array of the transformed RGBA-values</returns>
+        private static float[] floatFromColor(Color color) {
+            float[] rgbaFloat = new float[4];
+            rgbaFloat[0] = color.R / 255.0f;
+            rgbaFloat[1] = color.G / 255.0f;
+            rgbaFloat[2] = color.B / 255.0f;
+            rgbaFloat[3] = color.A / 255.0f;
+            return rgbaFloat;
+        }
+
+        /// <summary>
+        /// Calculates the Color from the given float-values.
+        /// </summary>
+        /// <param name="colors">Float-array of RGBA-values</param>
+        /// <returns>The transformed Color</returns>
+        private static Color colorFromFloat(float[] colors) {
+            byte r = (byte) (colors[0] * 255);
+            byte g = (byte) (colors[1] * 255);
+            byte b = (byte) (colors[2] * 255);
+            byte a = (byte) (colors[3] * 255);
+            return Color.FromArgb(a,r,g,b);
+        }
+
+        /// <summary>
+        /// Returns the color converted to System.Drawing.Color.
+        /// </summary>
+        /// <returns>The color as System.Drawing.Color</returns>
+        public Color ToColor() {
+            return Color.FromArgb((int)(rgbaFloat[0] * 255), (int)(rgbaFloat[1] * 255), (int)(rgbaFloat[2] * 255));
+        }
+
+        /// <summary>
+        /// Calculates the linear interpolation between two Colors,
+        /// this color being dist=0, the other being dist=1
+        /// </summary>
+        /// <param name="other">Other Color</param>
+        /// <param name="dist">Keep this between 0 and 1 please</param>
+        /// <returns>Interpolated Color</returns>
+        public ColorOGL Interpolate(ColorOGL other, float dist) {
+            ColorOGL c = new ColorOGL();
+            for (int i = 0; i < 4; i++)
+                c.rgbaFloat[i] = this.rgbaFloat[i] + (other.rgbaFloat[i] - this.rgbaFloat[i]) * dist;
+            c.color = colorFromFloat(c.rgbaFloat);
+            return c;
+        }
+
+        /// <summary>
+        /// Calculates a color table with values interpolated between
+        /// <paramref name="first"/> and <paramref name="second"/>
+        /// </summary>
+        /// <param name="first">Start-ColorOGL</param>
+        /// <param name="second">End-ColorOGL</param>
+        /// <returns>An array of interpolated ColorOGLs</returns>
+        public static ColorOGL[] InterpolationArray(ColorOGL first, ColorOGL second) {
+            ColorOGL[] colorTable = new ColorOGL[byte.MaxValue + 1];
+            for (int i = 0; i <= byte.MaxValue; i++) {
+                colorTable[i] = first.Interpolate(second, (float)i / byte.MaxValue);
+            }
+            return colorTable;
+        }
+
+        /// <summary>
+        /// Calculates a color table with values interpolated between
+        /// <paramref name="first"/> and <paramref name="second"/>
+        /// </summary>
+        /// <param name="first">Start-System.Drawing.Color</param>
+        /// <param name="second">End-System.Drawing.Color</param>
+        /// <returns>An array of interpolated ColorOGLs</returns>
+        public static ColorOGL[] InterpolationArray(Color first, Color second) {
+            ColorOGL firstOGL  = new ColorOGL(first);
+            ColorOGL secondOGL = new ColorOGL(second);
+            ColorOGL[] colorTable = new ColorOGL[byte.MaxValue + 1];
+            for (int i = 0; i <= byte.MaxValue; i++) {
+                colorTable[i] = firstOGL.Interpolate(secondOGL, (float)i / byte.MaxValue);
+            }
+            return colorTable;
+        }
+
+        /// <summary>
+        /// Calculates a color table with values interpolated between
+        /// <paramref name="first"/>, <paramref name="second"/> and <paramref name="third"/>.
+        /// </summary>
+        /// <param name="first">First System.Drawing.Color</param>
+        /// <param name="second">Second System.Drawing.Color</param>
+        /// <param name="third">Third System.Drawing.Color</param>
+        /// <returns>An array of interpolated ColorOGLs</returns>
+        public static ColorOGL[] InterpolationArray(Color first, Color second, Color third) {
+            ColorOGL firstOGL = new ColorOGL(first);
+            ColorOGL secondOGL = new ColorOGL(second);
+            ColorOGL thirdOGL = new ColorOGL(third);
+            ColorOGL[] colorTable = new ColorOGL[short.MaxValue + 1];
+            for (int i = 0; i <= short.MaxValue/2; i++) {
+                colorTable[i] = firstOGL.Interpolate(secondOGL, (float)(i*2) / short.MaxValue);
+            }
+            for (int i = short.MaxValue / 2; i <= short.MaxValue ; i++) {
+                colorTable[i] = secondOGL.Interpolate(thirdOGL, (float)((i*2-short.MaxValue*0.5) / short.MaxValue));
+            }
+            return colorTable;
+        }
+
+        /// <summary>
+        /// Overrides the ToString method to return the description of this ColorOGL.
+        /// </summary>
+        /// <returns>Description of this ColorOGL</returns>
+        public override string ToString() {
+            return Description;
+        }
+
+        #endregion
+    }
+}
